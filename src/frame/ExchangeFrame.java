a9a56d86f9d9cec477d800abef369fddda3223fe
Yuki Nagai <ynagai@cyber.t.u-tokyo.ac.jp>
2012-06-15 22:12:11 +0900

    JFreeChart

---
+++ src/frame/ExchangeFrame.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package frame;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+/**
+ * @author ynagai
+ *
+ */
+public class ExchangeFrame extends JFrame {
+    private static final long serialVersionUID = -21104437397340170L;
+    public ExchangeFrame() {
+        
+//        XYSeriesCollection dataset = new XYSeriesCollection();
+//        XYSeries series = new XYSeries("為替");
+//        series.add(1, 2);
+//        dataset.addSeries(series);
+//        JFreeChart chart = ChartFactory.createScatterPlot("為替相場履歴", "", "為替相場", dataset, PlotOrientation.VERTICAL, true, false, false);
+//        ChartPanel chartPanel = new ChartPanel(chart);
+        getContentPane().add(chartPanel, BorderLayout.CENTER);
+    }
+}
---
+++ src/main/ExchangeBrowser.java
@@ -0,0 +1,12 @@
+package main;
+
+import frame.ExchangeFrame;
+
+public class ExchangeBrowser {
+    public static void main(String[] args) {
+        ExchangeFrame frame = new ExchangeFrame();
+        frame.setTitle("Sample");
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
